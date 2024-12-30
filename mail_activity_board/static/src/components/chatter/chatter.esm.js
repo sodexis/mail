b49a3c70b7706a2cb7b07baa2fe8192baed3b677
arulraj <arulraj@sodexis.com>
2024-12-30 15:28:18 +0530

    [ADD] mail_activity_board

---
+++ mail_activity_board/static/src/components/chatter/chatter.esm.js
@@ -0,0 +1,34 @@
+import {Chatter} from "@mail/chatter/web_portal/chatter";
+import {patch} from "@web/core/utils/patch";
+
+patch(Chatter.prototype, {
+    // --------------------------------------------------------------------------
+    // Handlers
+    // --------------------------------------------------------------------------
+
+    /**
+     * @private
+     * @param {MouseEvent} ev
+     */
+    // eslint-disable-next-line no-unused-vars
+    async _onListActivity(ev) {
+        if (this.state.thread) {
+            const thread = this.state.thread;
+            const action = await this.orm.call(
+                thread.model,
+                "redirect_to_activities",
+                [[]],
+                {
+                    id: this.state.thread.id,
+                    model: this.state.thread.model,
+                }
+            );
+            this.action.doAction(action, {
+                onClose: () => {
+                    thread.refreshActivities();
+                    thread.refresh();
+                },
+            });
+        }
+    },
+});
